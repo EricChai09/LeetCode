ecb56d6d647e83b2be857183864e2a94430a732b
chaisheng <chaishengaa@163.com>
2019-02-21 00:01:30 +0800

    first commit

---
+++ LeetCode14.js
@@ -0,0 +1,44 @@
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+
+*/
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+     var str = "";
+    if(strs.length==0){
+         return str;
+    }else if(strs.length==0){
+        return strs[0]
+    }else{
+        var one = strs[0];
+        for(var i =0;i<one.length;i++){
+            for (var j =1;j<strs.length;j++){
+                if(one[i]!==strs[j][i]){
+                    return str;
+                }
+            }
+            str+=one[i]
+        }
+        
+    } 
+    
+    return str
+};
---
+++ LeetCode7.js
@@ -0,0 +1,44 @@
+/*
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+示例 1:
+
+输入: 123
+输出: 321
+ 示例 2:
+
+输入: -123
+输出: -321
+示例 3:
+
+输入: 120
+输出: 21
+注意:
+
+假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+*/
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    var str = x.toString().split('');
+    if(str[0]=="-"){
+        var num = parseInt(str.reverse().join(''));
+        if(-num<Math.pow(-2,31)){
+            return 0
+        }
+        return -num;
+    }else{
+        var num = parseInt(str.reverse().join(''));
+        if(num>(Math.pow(2,31)-1)){
+            return 0
+        }
+        return num;
+    }
+};

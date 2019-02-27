fc72d1697b29e1c17d5cefa6f2795e4f6f155e91
chaisheng <chaishengaa@163.com>
2019-02-27 22:36:13 +0800

    11/66

--- LeetCode11_66.js
+++ LeetCode11_66.js
@@ -1,27 +1,35 @@
 /*
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
 
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
 
-示例:
+你可以假设除了整数 0 之外，这个整数不会以零开头。
 
-给定 nums = [2, 7, 11, 15], target = 9
+示例 1:
 
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+示例 2:
+
+输入: [4,3,2,1]
+输出: [4,3,2,2]
+解释: 输入数组表示数字 4321。
 */
 
-
-
-var twoSum = function(nums, target) {
-    var a = [];
-    for (var i = 0;i<nums.length-1;i++){
-        for(var j = i+1 ; j<nums.length;j++){
-            if(nums[i]+nums[j] == target){
-                 a[0]=i;a[1]=j
-                 return a;
-            }
-           
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; --i) {
+        if (digits[i] === 9) {
+            digits[i] = 0
+        } else {
+            digits[i]++
+            return digits
         }
     }
+    digits.unshift(1)
+    return digits
 };

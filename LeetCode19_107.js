41b43848f77c15540a29bfdd7ce34bd70f78f845
chaisheng <chaishengaa@163.com>
2019-03-07 22:45:56 +0800

    19/107

---
+++ LeetCode19_107.js
@@ -0,0 +1,51 @@
+/*
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层次遍历为：
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if(root == null){
+        return []
+    }else{
+         var arry = [];
+        ok(root,0)
+        function ok(root, level){
+            if(!root) return;
+            if(arry.length <= level) arry.push([]);
+            var arrLevel = arry[level];
+            arrLevel.push(root.val);
+            ok(root.left,level+1);
+            ok(root.right,level+1);
+        }
+        return arry.reverse();
+    } 
+    
+};

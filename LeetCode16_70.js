0c409909e29955d306b3cc682f9c7a0cf4f6b74f
chaisheng <chaishengaa@163.com>
2019-03-12 22:02:03 +0800

    24/118

--- LeetCode16_70.js
+++ LeetCode16_70.js
@@ -25,10 +25,29 @@
 
 
 var climbStairs = function(n) {
-    if(n<=1){
-        return 1
-    }else{
-        return climbStairs(n-1) + climbStairs(n-2)
-    } 
+    if (n < 1) {
+      return 0
+    }
+  
+    if (n === 1) {
+      return 1
+    }
+  
+    if (n === 2) {
+      return 2
+    }
+  
+    var a = 1
+    var b = 2
+    var temp = 0
+  
+    for (var i = 3; i <= n; i++) {
+      temp = a + b
+      a = b
+      b = temp
+    }
+  
+    return temp
+};
     
 };
